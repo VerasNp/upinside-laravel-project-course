5284dee9fb6ba188fdb2e389a3d1031bece93a81
Vitor Veras <vitorveras01@gmail.com>
2021-02-16 02:23:41 -0500

    SEO with coffeecode/optimizer
    - Creating a way to abstract the SEO
    - Package used: coffeecode/optimizer

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,32 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\Seo;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function home()
     {
-        return view('front.home');
+        $head = $this->seo->render(env('APP_NAME') . ' - Upinside Treinamentos', 'Descrição teste', url('/'), asset('images/img/img_bg_1.jpg'));
+        return view('front.home', ['head' => $head]);
     }
 
     public function course()
     {
+        $head = $this->seo->render(env('APP_NAME') . ' - Sobre o curso', 'Descrição teste', route('course'), asset('images/img/img_bg_1.jpg'));
         return view('front.course');
     }
 
     public function blog()
     {
+        $head = $this->seo->render(env('APP_NAME') . ' - Blog', 'Descrição teste', route('blog'), asset('images/img/img_bg_1.jpg'));
         return view('front.blog');
     }
 
     public function article()
     {
+        $head = $this->seo->render(env('APP_NAME') . ' - Sobre', 'Descrição teste', url('/'), asset('images/img/img_bg_1.jpg'));
         return view('front.article');
     }
 
     public function contact()
     {
+        $head = $this->seo->render(env('APP_NAME') . ' - Upinside Treinamentos', 'Descrição teste', route('contact'), asset('images/img/img_bg_1.jpg'));
         return view('front.contact');
     }
 }
