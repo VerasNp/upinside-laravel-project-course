bec2a3267277d9951f243163160719416f6f173a
Vitor Veras <vitorveras01@gmail.com>
2021-02-16 03:15:12 -0500

    Laravel Mix 2
    - Using laravel mix to compilate sass files
    - Use copyDirectory to send images and fonts to the rigth path
    - Use of options and version

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,40 +11,57 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.styles(
-    [
-        "resources/views/front/assets/css/animate.css",
-        "resources/views/front/assets/css/icomoon.css",
-        "resources/views/front/assets/css/bootstrap.css",
-        "resources/views/front/assets/css/magnific-popup.min.css",
-        "resources/views/front/assets/css/owl.carousel.min.css",
-        "resources/views/front/assets/css/owl.theme.default.min.css",
-    ],
-    "public/front/assets/css/vendor.css"
+mix.sass(
+    "resources/views/front/assets/sass/style.scss",
+    "public/front/assets/css/style.css"
 )
+    .styles(
+        [
+            "resources/views/front/assets/css/animate.css",
+            "resources/views/front/assets/css/icomoon.css",
+            "resources/views/front/assets/css/bootstrap.css",
+            "resources/views/front/assets/css/magnific-popup.min.css",
+            "resources/views/front/assets/css/owl.carousel.min.css",
+            "resources/views/front/assets/css/owl.theme.default.min.css",
+        ],
+        "public/front/assets/css/vendor.css"
+    )
     .scripts(
-        ["resources/views/front/front/assets/js/modernizr-2.6.2.min.js"],
+        ["resources/views/front/assets/js/modernizr-2.6.2.min.js"],
         "public/front/assets/js/modernizr.js"
     )
     .scripts(
-        ["resources/views/front/front/assets/js/respond.min.js"],
-        "public/front/assets/js/respond.min.js"
+        ["resources/views/front/assets/js/respond.min.js"],
+        "public/front/assets/js/respond.js"
     )
     .scripts(
         [
-            "resources/views/front/front/assets/js/jquery.min.js",
-            "resources/views/front/front/assets/js/jquery.easing.1.3.js",
-            "resources/views/front/front/assets/js/bootstrap.min.js",
-            "resources/views/front/front/assets/js/jquery.waypoints.min.js",
-            "resources/views/front/front/assets/js/jquery.stellar.min.js",
-            "resources/views/front/front/assets/js/owl.carousel.min.js",
-            "resources/views/front/front/assets/js/jquery.countTo.js",
-            "resources/views/front/front/assets/js/jquery.magnific-popup.min.js",
-            "resources/views/front/front/assets/js/magnific-popup-options.js",
+            "resources/views/front/assets/js/jquery.min.js",
+            "resources/views/front/assets/js/jquery.easing.1.3.js",
+            "resources/views/front/assets/js/bootstrap.min.js",
+            "resources/views/front/assets/js/jquery.waypoints.min.js",
+            "resources/views/front/assets/js/jquery.stellar.min.js",
+            "resources/views/front/assets/js/owl.carousel.min.js",
+            "resources/views/front/assets/js/jquery.countTo.js",
+            "resources/views/front/assets/js/jquery.magnific-popup.min.js",
+            "resources/views/front/assets/js/magnific-popup-options.js",
         ],
         "public/front/assets/js/vendor.js"
     )
     .scripts(
-        ["resources/views/front/front/assets/js/main.js"],
+        ["resources/views/front/assets/js/main.js"],
         "public/front/assets/js/main.js"
-    );
+    )
+    .copyDirectory(
+        "resources/views/front/assets/fonts",
+        "public/front/assets/fonts"
+    )
+    .copyDirectory(
+        "resources/views/front/assets/images",
+        "public/front/assets/images"
+    )
+
+    .options({
+        processCssUrls: false,
+    })
+    .version();
