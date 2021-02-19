55b28386f9847bdf603641b59a83b14a34f0fd00
Vitor Veras <vitorveras01@gmail.com>
2021-02-19 15:45:10 -0500

    Blog and single post
    - Config of blog posts and return of the data of each post

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -24,14 +24,22 @@ class WebController extends Controller
 
     public function blog()
     {
+        $posts = \App\Post::orderBy('created_at', 'DESC')->limit(3)->get();
         $head = $this->seo->render(env('APP_NAME') . ' - Blog', 'Descrição teste', route('blog'), asset('images/img/img_bg_1.jpg'));
-        return view('front.blog');
+        return view('front.blog',  [
+            'posts' => $posts
+        ]);
     }
 
-    public function article()
+    public function article($uri)
     {
-        $head = $this->seo->render(env('APP_NAME') . ' - Sobre', 'Descrição teste', url('/'), asset('images/img/img_bg_1.jpg'));
-        return view('front.article');
+
+        $post = \App\Post::where('uri', $uri)->first();
+        $head = $this->seo->render(env('APP_NAME') . ' - ' . $post->title, $post->subtitle, route('article', $post->uri), \Illuminate\Support\Facades\Storage::url(\App\Support\Cropper::thumb($post->cover, 1200, 628)));
+        return view('front.article', [
+            'head' => $head,
+            'post' => $post
+        ]);
     }
 
     public function contact()
--- resources/views/front/article.blade.php
+++ resources/views/front/article.blade.php
@@ -2,49 +2,44 @@
 
 @section('content')
 
-    <header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner"
-            style="background-image:url(../front/assets/images/img_bg_2.jpg); height: 200px"
-            data-stellar-background-ratio="0.5">
-        <div class="overlay"></div>
-    </header>
+<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(../front/assets/images/img_bg_2.jpg); height: 200px" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+</header>
 
-    <div id="fh5co-blog">
-        <div class="container">
-            <div class="row animate-box">
-                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                    <h2>Título</h2>
-                    <p>Subtíulo</p>
-                </div>
+<div id="fh5co-blog">
+    <div class="container">
+        <div class="row animate-box">
+            <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                <h2>{{$post->title}}</h2>
+                <p>{{$post->subtitle}}</p>
             </div>
-            <div class="row">
-                <div class="fh5co-blog animate-box">
-                    <div class="col-12 text-center">
-                        <img style="max-width: 100%;"
-                             src="front/assets/images/project-4.jpg"
-                             alt="">
+        </div>
+        <div class="row">
+            <div class="fh5co-blog animate-box">
+                <div class="col-12 text-center">
+                    <img style="max-width: 100%;" src="{{\Illuminate\Support\Facades\Storage::url(\App\Support\Cropper::thumb($post->cover, 800, 450))}}" alt="">
+                </div>
+                <div class="col-12">
+                    <div class="blog-text">
+                        <span class="posted_on">{{date('d/m/Y H:i', strtotime($post->created_at))}}</span>
+                        <span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+                        <p>{{$post->content}}</p>
                     </div>
-                    <div class="col-12">
-                        <div class="blog-text">
-                            <span class="posted_on">01/01/2019</span>
-                            <span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
-                            <p>Conteúdo do artigo</p>
-                        </div>
-                    </div>
+                </div>
 
-                    <div class="col-12">
-                        <div class="fb-comments" data-href="url da página" data-numposts="5" data-width="100%"></div>
-                    </div>
+                <div class="col-12">
+                    <div class="fb-comments" data-href="url da página" data-numposts="5" data-width="100%"></div>
                 </div>
             </div>
         </div>
-    </div>s
+    </div>
+</div>s
 
-    @include('front.includes.optin')
+@include('front.includes.optin')
 
 @endsection
 
 @section('scripts')
-    <div id="fb-root"></div>
-    <script async defer
-            src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.2&appId=SEU_APP_ID&autoLogAppEvents=1"></script>
+<div id="fb-root"></div>
+<script async defer src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.2&appId=SEU_APP_ID&autoLogAppEvents=1"></script>
 @endsection
